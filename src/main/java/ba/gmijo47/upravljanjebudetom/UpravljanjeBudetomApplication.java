f5c2d762113269a40b8a4814fe05c620a5f94fea
gmijo47 <gmijo47@gmail.com>
2026-01-17 16:41:38 +0100

    Initialized project

---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/UpravljanjeBudetomApplication.java
@@ -0,0 +1,13 @@
+package ba.gmijo47.upravljanjebudetom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpravljanjeBudetomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpravljanjeBudetomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/gmijo47/upravljanjebudetom/UpravljanjeBudetomApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.gmijo47.upravljanjebudetom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpravljanjeBudetomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
