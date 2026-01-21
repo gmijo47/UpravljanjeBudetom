26bb5620d1c85dc06f5990c7e6f6611e964ad887
gmijo47 <gmijo47@gmail.com>
2026-01-21 22:31:02 +0100

    Add controller for categories, and service, modified sec config

--- src/main/java/ba/gmijo47/upravljanjebudetom/config/SecurityConfig.java
+++ src/main/java/ba/gmijo47/upravljanjebudetom/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN", "OWNER")
                                 .requestMatchers("/household/**").hasAnyRole("USER", "ADMIN", "OWNER")
+                                .requestMatchers("/category/**").hasAnyRole("USER", "ADMIN", "OWNER")
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(session -> session.disable())
