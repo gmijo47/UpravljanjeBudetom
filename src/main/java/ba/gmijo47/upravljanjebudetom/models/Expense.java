2fcf622a948876c10f854c1aeec2fb88d570b209
gmijo47 <gmijo47@gmail.com>
2026-01-21 23:10:21 +0100

    Add Expense repo & model

---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/models/Expense.java
@@ -0,0 +1,66 @@
+package ba.gmijo47.upravljanjebudetom.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonIgnore
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "monthly_budget_id")
+    @JsonIgnore
+    private MonthlyBudget monthlyBudget;
+
+    @Column
+    private BigDecimal amount;
+
+    @Column
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @Column
+    private String description;
+
+    public Expense() {}
+
+    public Expense(User user, Category category, MonthlyBudget monthlyBudget,
+                   BigDecimal amount, LocalDate date) {
+        this.user = user;
+        this.category = category;
+        this.monthlyBudget = monthlyBudget;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public Expense(User user, Category category, MonthlyBudget monthlyBudget,
+                   BigDecimal amount, LocalDate date, String description) {
+        this.user = user;
+        this.category = category;
+        this.monthlyBudget = monthlyBudget;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/repos/ExpenseRepo.java
@@ -0,0 +1,22 @@
+package ba.gmijo47.upravljanjebudetom.repos;
+
+import ba.gmijo47.upravljanjebudetom.models.Expense;
+import ba.gmijo47.upravljanjebudetom.models.Household;
+import ba.gmijo47.upravljanjebudetom.models.MonthlyBudget;
+import ba.gmijo47.upravljanjebudetom.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseRepo extends JpaRepository<Expense, Long> {
+    List<Expense> findByUser(User user);
+    List<Expense> findByMonthlyBudget(MonthlyBudget monthlyBudget);
+    @Query("SELECT e FROM Expense e JOIN e.monthlyBudget mb WHERE mb.household = :household")
+    List<Expense> findByHousehold(@Param("household") Household household);
+    @Query("SELECT e FROM Expense e JOIN e.monthlyBudget mb WHERE mb.household.id = :householdId")
+    List<Expense> findByHouseholdId(@Param("householdId") Long householdId);
+}
