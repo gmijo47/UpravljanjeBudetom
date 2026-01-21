70d31a25c6c42bec0c77e158a8148b767c6388bc
gmijo47 <gmijo47@gmail.com>
2026-01-21 22:22:57 +0100

    Add Category repo & model

---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/models/Category.java
@@ -0,0 +1,43 @@
+package ba.gmijo47.upravljanjebudetom.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "household_id")
+    @JsonIgnore
+    private Household household;
+
+    @Column
+    private String name;
+
+    @Column
+    private String type;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    @JsonIgnore
+    private List<Expense> expenses = new ArrayList<>();
+
+    public Category() {}
+
+    public Category(Household household, String name, String type) {
+        this.household = household;
+        this.name = name;
+        this.type = type;
+    }
+
+}
