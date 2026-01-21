87369bd15eb766dcc3609e079bac4e71cb748da6
gmijo47 <gmijo47@gmail.com>
2026-01-21 21:52:32 +0100

    Add Household Model & HouseholdRepo

---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/models/Household.java
@@ -0,0 +1,48 @@
+package ba.gmijo47.upravljanjebudetom.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "household")
+public class Household {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany
+    @JoinColumn(name = "household_id")
+    @JsonIgnore
+    private List<User> users = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "household_id")
+    @JsonIgnore
+    private List<MonthlyBudget> monthlyBudgets = new ArrayList<>();
+
+    @OneToMany
+    @JoinColumn(name = "household_id")
+    @JsonIgnore
+    private List<Category> categories = new ArrayList<>();
+
+    public Household() {}
+
+    public Household(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/ba/gmijo47/upravljanjebudetom/repos/HouseholdRepo.java
@@ -0,0 +1,19 @@
+package ba.gmijo47.upravljanjebudetom.repos;
+
+import ba.gmijo47.upravljanjebudetom.models.Household;
+import ba.gmijo47.upravljanjebudetom.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface HouseholdRepo extends JpaRepository<Household, Long> {
+    List<Household> findByUsersContaining(User user);
+    boolean existsByName(String name);
+    @Query("SELECT h FROM Household h JOIN h.users u JOIN u.roles r WHERE u.id = :userId AND r.name = 'ROLE_OWNER'")
+    Optional<Household> findByOwnerId(@Param("userId") Long userId);
+}
